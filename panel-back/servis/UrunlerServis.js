415e6f7a18158130a5f5b01e6e21d5a583b87484
MCanerD <mahircanerdogan@hotmail.com>
2022-05-30 13:53:19 +0300

    x

--- panel-back/servis/UrunlerServis.js
+++ panel-back/servis/UrunlerServis.js
@@ -1,5 +1,6 @@
 const {kahvalti,icecekler,TekBagel,Tatlilar,Sandvicler,HaftaSonuOzel,Ekstralar} = require("../Schemas/urunSchemas");
 
+
 const MenuAl = async ()  => {
   const Kahvaltı = await kahvalti.find({});
   const Icecekler = await icecekler.find({});
@@ -26,31 +27,56 @@ const MenuAl = async ()  => {
     haftaSonuObj: Hafta_Sonu_Ozel,
 
     ekstraObj: ekstralar,
-
+    
   }
-
+  
   return menuObj;
 };
 
-const update = (DuzenlenenUrun) => {   //Düzenlenmiş Ürün.
-  const guncelUrun = {
-    siparişler: DuzenlenenUrun.siparişler,
-    yemek: DuzenlenenUrun.yemek,
-    isim: DuzenlenenUrun.isim,
-    fiyat:DuzenlenenUrun.fiyat,
-    içerik:DuzenlenenUrun.içerik,
-    icecek:DuzenlenenUrun.icecek,
-    tatli:DuzenlenenUrun.tatli,
-    Urun:DuzenlenenUrun.Urun,
-    Fiyat:DuzenlenenUrun.Fiyat,
-    içindekiler:DuzenlenenUrun.içindekiler
-  };
-
-  return Urun.findByIdAndUpdate(DuzenlenenUrun.id, guncelUrun); //Id ile ürünü bulup düzenlenen ile değiştirme.
+const update = (DuzenlenenUrun) => {
+  
+  //Düzenlenmiş Ürün.
+  
+  
+};
+const guncelUrun = {
+  
+  siparişler: DuzenlenenUrun.siparişler,
+  
+  yemek: DuzenlenenUrun.yemek,
+  
+  isim: DuzenlenenUrun.isim,
+  
+  fiyat: DuzenlenenUrun.fiyat,
+  
+  içerik: DuzenlenenUrun.içerik,
+  
+  icecek: DuzenlenenUrun.icecek,
+  
+  tatli: DuzenlenenUrun.tatli,
+  
+  Urun: DuzenlenenUrun.Urun,
+  
+  Fiyat: DuzenlenenUrun.Fiyat,
+  
+  içindekiler: DuzenlenenUrun.içindekiler,
+  
 };
 
-const remove = (silinecekUrun) => {
-  return Urun.findByIdAndDelete(silinecekUrun.id); //Id ile ürünü bulup silme.
+switch(tabIndex){
+  case 1: 
+ Tatlilar.findByIdAndUpdate(DuzenlenenUrun.id, guncelUrun); //Id ile ürünü bulup düzenlenen ile değiştirme.
+  break
+  case 2:
+   icecekler.findByIdAndUpdate(DuzenlenenUrun.id, guncelUrun); //Id ile ürünü bulup düzenlenen ile değiştirme.
+  break;
+  // return Tatlilar.findByIdAndUpdate(DuzenlenenUrun.id, guncelUrun); //Id ile ürünü bulup düzenlenen ile değiştirme.
+  
+};
+
+
+const remove = (UrunSil) => {
+  return Tatlilar.findByIdAndDelete(UrunSil.id); //Id ile ürünü bulup silme.
 };
 
 const list = () => {
