bcc7d8ea43c0b2e18442c809047f1a8b822e06e3
Can Razboyalı <canrazbonyali@gmail.com>
2022-05-30 14:16:26 +0300

    Ufak ekleme (switch-case)

--- panel-back/servis/UrunlerServis.js
+++ panel-back/servis/UrunlerServis.js
@@ -1,7 +1,14 @@
-const {kahvalti,icecekler,TekBagel,Tatlilar,Sandvicler,HaftaSonuOzel,Ekstralar} = require("../Schemas/urunSchemas");
+const {
+  kahvalti,
+  icecekler,
+  TekBagel,
+  Tatlilar,
+  Sandvicler,
+  HaftaSonuOzel,
+  Ekstralar,
+} = require("../Schemas/urunSchemas");
 
-
-const MenuAl = async ()  => {
+const MenuAl = async () => {
   const Kahvaltı = await kahvalti.find({});
   const Icecekler = await icecekler.find({});
   const Tek_Bagel = await TekBagel.find({});
@@ -10,14 +17,11 @@ const MenuAl = async ()  => {
   const Hafta_Sonu_Ozel = await HaftaSonuOzel.find({});
   const ekstralar = await Ekstralar.find({});
 
-
-
   const menuObj = {
-
     kahvaltiObj: Kahvaltı,
-    
+
     icecekObj: Icecekler,
-    
+
     bagelObj: Tek_Bagel,
 
     tatliObj: tatlilar,
@@ -27,53 +31,39 @@ const MenuAl = async ()  => {
     haftaSonuObj: Hafta_Sonu_Ozel,
 
     ekstraObj: ekstralar,
-    
-  }
-  
+  };
+
   return menuObj;
 };
 
 const update = (DuzenlenenUrun) => {
-  
   //Düzenlenmiş Ürün.
-  
-  
-};
-const guncelUrun = {
-  
-  siparişler: DuzenlenenUrun.siparişler,
-  
-  yemek: DuzenlenenUrun.yemek,
-  
-  isim: DuzenlenenUrun.isim,
-  
-  fiyat: DuzenlenenUrun.fiyat,
-  
-  içerik: DuzenlenenUrun.içerik,
-  
-  icecek: DuzenlenenUrun.icecek,
-  
-  tatli: DuzenlenenUrun.tatli,
-  
-  Urun: DuzenlenenUrun.Urun,
-  
-  Fiyat: DuzenlenenUrun.Fiyat,
-  
-  içindekiler: DuzenlenenUrun.içindekiler,
-  
-};
 
-switch(tabIndex){
-  case 1: 
- Tatlilar.findByIdAndUpdate(DuzenlenenUrun.id, guncelUrun); //Id ile ürünü bulup düzenlenen ile değiştirme.
-  break
-  case 2:
-   icecekler.findByIdAndUpdate(DuzenlenenUrun.id, guncelUrun); //Id ile ürünü bulup düzenlenen ile değiştirme.
-  break;
-  // return Tatlilar.findByIdAndUpdate(DuzenlenenUrun.id, guncelUrun); //Id ile ürünü bulup düzenlenen ile değiştirme.
-  
-};
+  console.log("Service update - ", DuzenlenenUrun);
+  const guncelUrun = {
+    siparişler: DuzenlenenUrun.siparişler,
 
+    yemek: DuzenlenenUrun.yemek,
+    isim: DuzenlenenUrun.isim,
+    fiyat: DuzenlenenUrun.fiyat,
+    içerik: DuzenlenenUrun.içerik,
+    icecek: DuzenlenenUrun.icecek,
+    tatli: DuzenlenenUrun.tatli,
+    Urun: DuzenlenenUrun.Urun,
+    Fiyat: DuzenlenenUrun.Fiyat,
+    içindekiler: DuzenlenenUrun.içindekiler,
+  };
+
+  switch (DuzenlenenUrun.tabIndex) {
+    case 1:
+      return Tatlilar.findByIdAndUpdate(DuzenlenenUrun.id, guncelUrun); //Id ile ürünü bulup düzenlenen ile değiştirme.
+    //break;
+    case 2:
+      return icecekler.findByIdAndUpdate(DuzenlenenUrun.id, guncelUrun); //Id ile ürünü bulup düzenlenen ile değiştirme.
+    //break;
+    // return Tatlilar.findByIdAndUpdate(DuzenlenenUrun.id, guncelUrun); //Id ile ürünü bulup düzenlenen ile değiştirme.
+  }
+};
 
 const remove = (UrunSil) => {
   return Tatlilar.findByIdAndDelete(UrunSil.id); //Id ile ürünü bulup silme.
@@ -87,5 +77,5 @@ module.exports = {
   MenuAl,
   update,
   remove,
-  list
+  list,
 };
