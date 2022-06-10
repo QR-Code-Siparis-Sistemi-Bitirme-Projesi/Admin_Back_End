5df569b368187f93c47a687eb4cb34c1d84b6e9d
MCanerD <mahircanerdogan@hotmail.com>
2022-06-10 15:17:58 +0300

    tabIndex olmadan hata görüntülnmeme sorunu çözüldü

--- panel-back/middleware/dogrulama.js
+++ panel-back/middleware/dogrulama.js
@@ -153,11 +153,14 @@ const secmeliValidasyon = (req, res, next) => {
 
         break;
     }
+    return next();
   }
 
-  // res.status(500).send();
+  res.send({
+    hataMesaji: "Hangi menü tablosunda olacağını belirtiniz..",
+    status: httpStatus.BAD_REQUEST,
+  });
 
-  return next();
 };
 
 const urunlerValidation = (schema) => (req, res, next) => {
